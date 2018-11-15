187529847ab37359a84e7a825b392fbd8ae1f0ea
avigail18-meet <tutim2000@gmail.com>
2018-11-15 18:47:43 +0200

    dfg

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+
 from flask import Flask
 from flask import render_template
+from flask import request, redirect, url_for
 from database import *
 
 app = Flask(__name__)
@@ -15,11 +17,14 @@ def cat_page(id):
 	cat = my_cat(id)
 	return render_template("cat.html", id=id , cat=cat)
 
-@app.route('/catnew/<string:name>')
-def new_cat(name):
-	new_cat= create_cat(name)
-	return render_template("cat.html", name=name)
-
+@app.route('/catnew', methods=["GET", "POST"])
+def new_cat():
+    if request.method == 'GET':
+        return render_template("new_cat.html")
+    else:
+    	name = request.form['firstname']
+        create_cat(name)
+        return redirect(url_for("catbook_home"))
 
 
 if __name__ == '__main__':
